84b7469be158952318513f99af32ee7a9a388d38
Abhinav Sharma <sharmaabhinav247176@gmail.com>
2023-05-20 22:28:54 +0530

    20-05

---
+++ diamondPattern.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class diamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            //spaces
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //first half, stars - 2*i-1
+            for (j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+
+            System.out.println();
+        }
+        for (i = n; i >= 1; i--) {
+            //spaces
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //first half, backward loop from i to 1
+            for (j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+
+            System.out.println();
+        }
+
+
+    }
+}
+
+/*
+ *
+ ***
+ *****
+ *******
+ *******
+ *****
+ ***
+ *
+ */
---
+++ numberPyramid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class numberPyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
+/*
+    1
+   2 2
+  3 3 3
+ 4 4 4 4
+5 5 5 5 5
+ */
